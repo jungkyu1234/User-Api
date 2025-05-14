9b3f373038ec519cec50014b303be43e26c78fcf
jungkyu1234 <78792506+jungkyu1234@users.noreply.github.com>
2025-05-14 10:41:35 +0900

    Delete src/main directory

--- src/main/java/com/zerobase/cms/domain/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zerobase.cms.domain.repository;
-
-import com.zerobase.cms.domain.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
